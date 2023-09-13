77daa893d1dfcf4303ce88be769b7c6d97a05a27
Luis Gaudencio <luis_g24@live.com>
2023-09-13 21:08:26 +0000

    Next commit

--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,26 @@
 let game = {
     currentGame: [],
-    score: 0,
     playerMoves: [],
-    choices: ["button1", "button2", "button3", "button4"],
+    score: 0,
+    choices: ["button1", "button2", "button3", "button4"]
 };
 
+function newGame() {
+    game.currentGame = [];
+    game.playerMoves = [];
+    game.score = 0;
+    showScore();
+    addTurn();
+}
 
-module.exports = { game };
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn };
